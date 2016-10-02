5121c242e29327fd18bb895b5b5ec407357999d9
root <mickeyyf@gmail.com>
2016-10-02 01:26:33 -0400

    encode change

--- encryption.py
+++ encryption.py
@@ -1,5 +1,6 @@
-#判断 传入的是什么类型
-#已经传出时类型是否改变
+# if last bit is 0 then write 16 zero again
+# when decode if lenght of zero > 16 only remove 16 zero
+#
 #
 #
 from Crypto.Cipher import AES
@@ -7,20 +8,53 @@ from Crypto.Cipher import AES
 _objEncryption = None
 
 def do16BitMultiple(szMessage):
+    return do16BitMultiple_2(szMessage)
+
+def do16BitMultiple_1(szMessage):
     length = 16 - (len(szMessage)%16)
     if length != 16:
         if type(szMessage).__name__ == "str":
-            return szMessage + "\0"*length
+            return szMessage + "\x04" + "\x00"*(length-1)
         else:
-            return szMessage + b'\0'*length
+            return szMessage + b'\x04' + b'\x00'*(length-1)
     else:
         return szMessage
 
-def deleteNullChart(szMessage):
+def do16BitMultiple_2(szMessage):
+    length = 16 - (len(szMessage)%16)
     if type(szMessage).__name__ == "str":
-        return szMessage.rstrip("\0")
+        return szMessage + "\x04" + "\x00"*(length-1)
     else:
-        return szMessage.rstrip(b'\0')
+        return szMessage + b'\x04' + b'\x00'*(length-1)
+
+def deleteNullChart(szMessage):
+    return deleteNullChart_2(szMessage)
+
+def deleteNullChart_1(szMessage):
+    if type(szMessage).__name__ == "str":
+        index = szMessage.rfind("\x04")
+        #index = szMessage.find("\x00")
+    else:
+        index = szMessage.rfind(b'\x04')
+        #index = szMessage.find(b'\x00')
+    print("debug:")
+    print(index)
+    if (index != -1):
+        return szMessage[:index]
+    else: 
+        return szMessage
+
+def deleteNullChart_2(szMessage):
+    if type(szMessage).__name__ == "str":
+        raise ValueError
+        print("-------------------str-------------------")
+        szOutput=  szMessage.rstrip("\x00")
+        if szOutput[-1] == 0x04:
+            return szOutput[:-1]
+    else:
+        szOutput=  szMessage.rstrip(b'\x00')
+        if szOutput[-1] == 0x04:
+            return szOutput[:-1]
 
 def setAESEncryptionKey(szKey):
     global _objEncryption
@@ -42,45 +76,38 @@ def decrypt(szMessage):
     else:
         raise AssertionError("must call setAESEncryptionKey(szkey) first")
 
+# test case
+def doTest(szTestSrt):
+    sz16Multiple = do16BitMultiple(szTestSrt)
+    szdelNull = deleteNullChart(sz16Multiple)
+    szEncode = encrypt(szTestSrt)
+    szDecode = decrypt(szEncode)
+
+    print("=======================start==================")
+    print("test srting ->{}".format(szTestSrt))
+    print("16 Multiple ->{}".format(sz16Multiple))
+    print("len         ->{}".format(len(sz16Multiple)))
+    print("del null    ->{}".format(szdelNull))
+    print("len         ->{}".format(len(szdelNull)))
+    print("encode      ->{}".format(szEncode))
+    print("len         ->{}".format(len(szEncode)))
+    print("decode      ->{}".format(szDecode))
+    print("len         ->{}".format(len(szDecode)))
+    print("=====================end======================")
+
+
 if __name__ == "__main__":
-    try:
-        print(encrypt("szMessage"))
-    except Exception:
-        setAESEncryptionKey("0123456789abcdef")
-        szString = encrypt("1")
-        print(szString)
-        print(decrypt(szString))
-        print(len(decrypt(szString)))
-
-        szString = encrypt("17-3456789abcdef0")
-        print(szString)
-        print(decrypt(szString))
-        print(len(decrypt(szString)))
-
-        szString = encrypt("15-asdfghjklqwe")
-        print(szString)
-        print(decrypt(szString))
-        print(len(decrypt(szString)))
-
-        szString = encrypt("31-qwertyuiopasdfghjklzxcvbnm12")
-        print(szString)
-        print(decrypt(szString))
-        print(len(decrypt(szString)))
-
-        szString = encrypt(b"16-zxcvbnm123456")
-        print(szString)
-        print(decrypt(szString))
-        print(len(decrypt(szString)))
-
-        szString = encrypt(b"32-1234567890qwertyuioplkjhgfdsa")
-        print(szString)
-        print(decrypt(szString))
-        print(len(decrypt(szString)))
-        
-        szMessage = b'start\0\0\0\0'
-        szMessage.rstrip(b"\0")
-        print(szMessage)
-        print(do16BitMultiple("abc"))
-        print(do16BitMultiple(b"def"))
-        print(deleteNullChart("abc\0\0\0\0"))
-        print(deleteNullChart(b"start\0\0\0\0"))
+    setAESEncryptionKey("0123456789abcdef")
+    #doTest("1")
+    #doTest("17-3456789abcdef0")
+    #doTest("15-asdfghjklqwe")
+    #doTest("31-qwertyuiopasdfghjklzxcvbnm12")
+    #doTest("16-zxcvbnm123456")
+    #doTest("32-1234567890qwertyuioplkjhgfdsa")
+    print("===============bytes test=====================")
+    doTest(b"1")
+    doTest(b"17-3456789abcdef0")
+    doTest(b"15-asdfghjklqwe")
+    doTest(b"31-qwertyuiopasdfghjklzxcvbnm12")
+    doTest(b"16-zxcvbnm123456")
+    doTest(b"32-1234567890qwertyuioplkjhgfdsa")
